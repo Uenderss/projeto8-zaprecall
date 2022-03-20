45c6d67f35d70d93309cb57ba1c0080182f182df
Uenderss <meajudabarto@gmail.com>
2022-03-20 16:32:43 -0300

    todos flashcard foram respondido

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -2,12 +2,15 @@ import React from "react";
 import zap from '../assets/zap.png';
 import quase from '../assets/quase.png';
 import errou from '../assets/errou.png';
+import sad from '../assets/sad.png';
+import party from '../assets/party.png'
+
 
 
 export default function Footer(props) {
   
   let lista=[...props.item];
-
+  
   
   function verificarIcone(icone){
     if(icone === 'zap'){
@@ -21,14 +24,30 @@ export default function Footer(props) {
     }
 
   }
- 
+
+  let imgSaudacao, saudacao, texto;
+
+  if(lista.length === 8 && lista.indexOf('errou')){
+      imgSaudacao =sad;
+      saudacao="Putz";
+      texto=`Ainda faltam alguns...
+             Mas não desamime`;
+  }
+  if(lista.length === 8 && !lista.indexOf('errou')){
+    imgSaudacao =party;
+    saudacao="Parabéns";
+    texto=`Você não esqueceu de
+           nenhum flashcard!`;
+}
+
   return (
     <footer>
+        <div className="status"><img src={imgSaudacao} alt="" /><strong>{saudacao}</strong></div>
+        <div className="resultado"><span>{texto}</span></div>
         <h4>
           {props.item.length}/{props.total} CONCLUÍDOS <br/>
           {
-            lista.map((item)=><img src={verificarIcone(item)} alt={item}/>
-                   
+            lista.map((item)=><img src={verificarIcone(item)} alt={item}/>        
           )}
         </h4>
 
